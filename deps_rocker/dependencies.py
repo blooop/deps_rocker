66b89ef71852a873c5243147dbee6ba16c72518a
Austin Gregg-Smith <blooop@gmail.com>
2024-01-28 14:23:07 +0000

    add docstrings

--- deps_rocker/dependencies.py
+++ deps_rocker/dependencies.py
@@ -4,26 +4,39 @@ from rocker.extensions import RockerExtension
 from pathlib import Path
 import yaml
 import toml
+from collections import defaultdict
 
 class Dependencies(RockerExtension):
 
     name = 'deps_dependencies'
 
+    def __init__(self) -> None:
+        self.dependencies= defaultdict(set)
+        super().__init__()
+
     @classmethod
     def get_name(cls):
         return cls.name
 
-    def precondition_environment(self, cli_args):
-        pass
-
     def read_dependencies(self):
-        deps_paths = Path.cwd().glob("deps.yaml")
-        self.dependencies={}
-        for path in deps_paths:
-            with open(path, 'r') as file:
-                self.dependencies.update(yaml.safe_load(file))
+        """Recursivly load all deps.yaml and create a dictionary containing sets of each type of dependency. Each type of dependency (apt_base, apt etc) should have duplicates rejected when adding to the set"""
+       
+        for path in Path.cwd().rglob("deps.yaml"):
+            with open(path, 'r',encoding="utf-8") as file:
+                vals = yaml.safe_load(file)
+                for k in vals:                
+                    for v in vals[k]:
+                        self.dependencies[k].add(v)
 
-    def get_deps(self,key)->str:
+    def get_deps(self,key:str)->str:
+        """Given a dependency key return a space delimited string of dependencies
+
+        Args:
+            key (str): A type of dependency e.g apt
+
+        Returns:
+            str: space delimited dependencies
+        """
         if key in self.dependencies:
             return " ".join(self.dependencies[key])
         return ""
@@ -51,10 +64,15 @@ class Dependencies(RockerExtension):
         return all_files
 
     def get_pyproject_toml_deps(self)->str:
-        pp_toml = Path.cwd().glob("pyproject.toml")
+        """Recursivly load all dependencies from pyproject.toml"
+
+        Returns:
+            str: Space delimited string of dependencies
+        """
+        pp_toml = Path.cwd().rglob("pyproject.toml")
         deps = []
         for p in pp_toml:
-            with open(p, 'r') as f:
+            with open(p, 'r',encoding="utf-8") as f:
                 config = toml.load(f)
                 project = config["project"]
                 if "dependencies" in project:
@@ -68,24 +86,19 @@ class Dependencies(RockerExtension):
                     
         return " ".join(deps)
 
-    def get_preamble(self, cli_args):
-        return ''
-
-    def get_snippet(self, cli_args):
+    def get_snippet(self, cliargs):
         snippet = pkgutil.get_data(
             'deps_rocker',
             'templates/dependencies_snippet.Dockerfile').decode('utf-8')
         return em.expand(snippet)
 
-    def get_docker_args(self, cli_args):
-        return ''
 
     @staticmethod
-    def register_arguments(parser,defaults={}):
+    def register_arguments(parser,defaults=None):
         parser.add_argument('--deps-dependencies',
             action='store_true',
             help='install deps.yaml ')
 
 if __name__ == "__main__":
-    res =Dependencies().get_pyproject_toml_deps()
+    res =Dependencies().get_files(None)
     print(res)
