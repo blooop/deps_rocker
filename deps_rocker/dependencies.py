3e68dd507ad56a44c8145b8dcb166673ac0f87c6
Austin Gregg-Smith <blooop@gmail.com>
2024-04-04 19:31:50 +0100

    Merge commit '91a97f75a62fd542f7e68e88f356ff0e7b59980b'

--- deps_rocker/dependencies.py
+++ deps_rocker/dependencies.py
@@ -40,28 +40,58 @@ class Dependencies(RockerExtension):
         if key in self.dependencies:
             return " ".join(self.dependencies[key])
         return ""
-
-    def get_files(self, cliargs):
-        all_files = {}
-
-        self.read_dependencies()
-        deps_names = ["apt_base","pip_base","apt"]
-        for dep in deps_names:
-            filename = f"{dep}.deps"
-            if dep in self.dependencies:
-                all_files[filename] = self.get_deps(dep)
-            else:
-                all_files[filename] = ""
-
-        pip_deps = self.get_deps("pip") +" "+ self.get_pyproject_toml_deps()
+    
+    def get_pips_deps(self,key:str)->str:
+        pip_deps = self.get_deps(key)
+        print(pip_deps)
 
         #todo remove this hack
         if pip_deps =="":
             pip_deps = "pip"
+        return pip_deps
+        
+    def get_files(self, cliargs)->dict:
+        all_files = {}
 
-        all_files["pip.deps"] =  pip_deps
+        self.read_dependencies()
+        #apt deps
+        deps_names = ["apt_tools","apt_base","apt"]
+        for dep in deps_names:
+            all_files[f"{dep}.deps"]= self.get_deps(dep)
+
+        #pip deps
+        deps_names = ["pip_tools","pip_base"]
+        for dep in deps_names:
+            all_files[f"{dep}.deps"]=self.get_pips_deps(dep) 
+
+        all_files["pip.deps"] =  self.get_pips_deps("pip") +" "+ self.get_pyproject_toml_deps()
+
+        #setup custom scripts
+        all_files["scripts_tools.sh"] = self.get_scripts("scripts_tools")
+        all_files["scripts_base.sh"] = self.get_scripts("scripts_base")
+        all_files["scripts.sh"] = self.get_scripts("scripts")
+        all_files["scripts_post.sh"] = self.get_scripts("scripts_post")
 
         return all_files
+    
+    def get_scripts(self,name:str)->str:
+        """collect all scripts files into a single script
+
+        Args:
+            name (str): name of the scripts key and output script name
+        Returns:
+            str: All scripts combined into a single script
+        """
+        #make sure the first line has shebang
+        scripts = ["#! /bin/bash"]
+        scripts_deps = self.get_deps(name)
+        if len(scripts_deps)>0:
+            for s in scripts_deps.split(" "):
+                with open(s,encoding="utf-8") as f:
+                    scripts.extend(f.readlines())
+
+        return "\n".join(scripts) 
+    
 
     def get_pyproject_toml_deps(self)->str:
         """Recursivly load all dependencies from pyproject.toml"
--- deps_rocker/templates/dependencies_snippet.Dockerfile
+++ deps_rocker/templates/dependencies_snippet.Dockerfile
@@ -1,15 +1,39 @@
-COPY apt_base.deps apt_base.deps
+#INSTALL DEVELOPMENT TOOLS
+COPY scripts_tools.sh /scripts_tools.sh
+RUN chmod +x /scripts_tools.sh; /scripts_tools.sh
+
+COPY apt_tools.deps /apt_tools.deps
+RUN apt-get update \ 
+ && apt-get install -y $(cat /apt_tools.deps) \
+ && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+COPY pip_tools.deps /pip_tools.deps
+RUN pip3 install -U $(cat /pip_tools.deps)
+
+#INSTALL EXPENSIVE BASE DEPENDENCIES
+COPY scripts_base.sh /scripts_base.sh
+RUN chmod +x /scripts_base.sh; /scripts_base.sh
+
+COPY apt_base.deps /apt_base.deps
 RUN apt-get update \ 
  && apt-get install -y $(cat /apt_base.deps) \
- && apt-get clean 
+ && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-COPY pip_base.deps pip_base.deps
-RUN pip install -U $(cat /pip_base.deps)
+COPY pip_base.deps /pip_base.deps
+RUN pip3 install -U $(cat /pip_base.deps)
 
-COPY apt.deps apt.deps
+#INSTALL DEVELOPMENT DEPENDENCIES
+COPY scripts.sh /scripts.sh
+RUN chmod +x /scripts.sh; /scripts.sh
+
+COPY apt.deps /apt.deps
 RUN apt-get update \ 
  && apt-get install -y $(cat /apt.deps) \
- && apt-get clean 
+ && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-COPY pip.deps pip.deps
-RUN pip install -U $(cat /pip.deps)
+COPY pip.deps /pip.deps
+RUN pip3 install -U $(cat /pip.deps)
+
+#POST SETUP
+COPY scripts_post.sh /scripts_post.sh
+RUN chmod +x /scripts_post.sh; /scripts_post.sh
