dd14fc4493dc01f4805934f82f0b6ed7b6d1f113
Austin Gregg-Smith <blooop@gmail.com>
2024-12-07 18:09:44 +0000

    involk after vcstool

--- deps_rocker/ros_humble.py
+++ deps_rocker/ros_humble.py
@@ -5,6 +5,9 @@ from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 class RosHumble(SimpleRockerExtension):
     name = "ros_humble"
 
+    def invoke_after(self, cliargs):
+        return set(["vcstool"])
+
     @staticmethod
     def register_arguments(parser, defaults=None):
         return SimpleRockerExtension.register_arguments_helper(RosHumble.name, parser, defaults)
