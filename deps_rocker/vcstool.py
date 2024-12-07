2c893947110e688bde7e030e0db26b23d7d0fe5e
Austin Gregg-Smith <blooop@gmail.com>
2024-12-07 18:46:11 +0000

    not working

--- deps_rocker/vcstool.py
+++ deps_rocker/vcstool.py
@@ -8,10 +8,9 @@ class VcsTool(SimpleRockerExtension):
     name = "vcstool"
 
     def __init__(self) -> None:
-        self.empy_args = {}
-        self.empy_args["depend_repos"] = []
-        self.output_files = self.generate_files()
         super().__init__()
+        self.empy_user_args["depend_repos"] = []
+        self.output_files = self.generate_files()
 
     def generate_files(self):
         """Generates depend.repos files and collects their paths
@@ -26,28 +25,16 @@ class VcsTool(SimpleRockerExtension):
                 with r.open(encoding="utf-8") as f:
                     rel_path = r.relative_to(Path.cwd()).as_posix()
                     output_files[rel_path] = f.read()
-                    self.empy_args["depend_repos"].append(
+                    self.empy_user_args["depend_repos"].append(
                         dict(dep=rel_path, path=Path(rel_path).parent.as_posix())
                     )
         return output_files
-
-    def get_snippet(self, cliargs):
-        snippet = pkgutil.get_data(
-            "deps_rocker", f"templates/{self.name}_snippet.Dockerfile"
-        ).decode("utf-8")
-
-        print("empy_snippet", snippet)
-        print("empy_data", self.empy_args)
-        expanded = em.expand(snippet, self.empy_args)
-
-        print("expanded\n", expanded)
-        return expanded
 
     def get_files(self, cliargs) -> dict:
         return self.output_files
 
     def invoke_after(self, cliargs):
-        return set(["cwd"])
+        return set(["cwd", "user"])
 
     @staticmethod
     def register_arguments(parser, defaults=None):
