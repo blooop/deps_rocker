0d0b0ba806248c5514a0a88c26362fdf5c028e7e
Austin Gregg-Smith <blooop@gmail.com>
2024-12-07 17:46:31 +0000

    update isaacsim to use the SimpleRockerExtension

--- deps_rocker/isaac_sim.py
+++ deps_rocker/isaac_sim.py
@@ -1,26 +1,14 @@
 import pkgutil
 from typing import Set
 from rocker.extensions import RockerExtension
+from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 
 # to run:
 # isaacsim omni.isaac.sim.python.kit
 
-class IsaacSim(RockerExtension):
-    name = "isaac_sim"
 
-    @classmethod
-    def get_name(cls):
-        return cls.name
-
-    def get_snippet(self, cliargs):
-        return pkgutil.get_data("deps_rocker", f"templates/{self.name}_snippet.Dockerfile").decode(
-            "utf-8"
-        )
-
-    # def get_user_snippet(self, cliargs):
-    #     return pkgutil.get_data("deps_rocker", f"templates/{self.name}_user.Dockerfile").decode(
-    #         "utf-8"
-    #     )
+class IsaacSim(SimpleRockerExtension):
+    name = "isaacsim"
 
     def required(self, cliargs) -> Set[str]:
         return set(["nvidia", "privileged", "x11"])
@@ -30,11 +18,4 @@ class IsaacSim(RockerExtension):
 
     @staticmethod
     def register_arguments(parser, defaults=None):
-        if defaults is None:
-            defaults = {}
-        parser.add_argument(
-            f"--isaac-sim",
-            action="store_true",
-            default=defaults.get("deps_rocker"),
-            help="add isaac_sim to your docker image",
-        )
+        SimpleRockerExtension.register_arguments_helper(IsaacSim.name, parser, defaults)
