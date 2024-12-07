11b685ad191edb442880257514f519040f380799
Austin Gregg-Smith <blooop@gmail.com>
2024-12-07 17:51:43 +0000

    use simple rocker extension

--- deps_rocker/ros_humble.py
+++ deps_rocker/ros_humble.py
@@ -1,31 +1,10 @@
 import pkgutil
-from rocker.extensions import RockerExtension
+from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 
 
-class RosHumble(RockerExtension):
+class RosHumble(SimpleRockerExtension):
     name = "ros_humble"
-
-    @classmethod
-    def get_name(cls):
-        return cls.name
-
-    def get_snippet(self, cliargs):
-        return pkgutil.get_data("deps_rocker", f"templates/{self.name}_snippet.Dockerfile").decode(
-            "utf-8"
-        )
-
-    def get_user_snippet(self, cliargs):
-        return pkgutil.get_data(
-            "deps_rocker", f"templates/{self.name}_snippet_user.Dockerfile"
-        ).decode("utf-8")
 
     @staticmethod
     def register_arguments(parser, defaults=None):
-        if defaults is None:
-            defaults = {}
-        parser.add_argument(
-            "--ros-humble",
-            action="store_true",
-            default=defaults.get("deps_rocker"),
-            help="add ros humble to your docker image",
-        )
+        return SimpleRockerExtension.register_arguments_helper(RosHumble.name, parser, defaults)
