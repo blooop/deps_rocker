3cc16a183f0f82ab003e825d94505dae61ce20c0
Austin Gregg-Smith <blooop@gmail.com>
2024-12-10 15:47:44 +0000

    add uv and curl extensions

--- deps_rocker/curl.py
+++ deps_rocker/curl.py
@@ -2,9 +2,9 @@ from pathlib import Path
 from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 
 
-class VcsTool(SimpleRockerExtension):
+class Curl(SimpleRockerExtension):
     name = "curl"
 
     @staticmethod
     def register_arguments(parser, defaults=None):
-        SimpleRockerExtension.register_arguments_helper(VcsTool.name, parser, defaults)
+        SimpleRockerExtension.register_arguments_helper(Curl.name, parser, defaults)
--- deps_rocker/uv.py
+++ deps_rocker/uv.py
@@ -2,12 +2,15 @@ from pathlib import Path
 from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 
 
-class VcsTool(SimpleRockerExtension):
+class UV(SimpleRockerExtension):
     name = "uv"
+
+    def required(self, cliargs):
+        return set(["curl"])
 
     def invoke_after(self, cliargs):
         return set(["curl"])
 
     @staticmethod
     def register_arguments(parser, defaults=None):
-        SimpleRockerExtension.register_arguments_helper(VcsTool.name, parser, defaults)
+        SimpleRockerExtension.register_arguments_helper(UV.name, parser, defaults)
