c386fb6502051838431e279b16814d691209a981
Austin Gregg-Smith <blooop@gmail.com>
2024-12-14 13:17:19 +0000

    adds git clone

---
+++ deps_rocker/git_clone.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from deps_rocker.simple_rocker_extension import SimpleRockerExtension
+
+
+class GitClone(SimpleRockerExtension):
+    """Adds support for git cloning"""
+
+    name = "git_clone"
+
---
+++ deps_rocker/templates/fzf_snippet.Dockerfile
@@ -0,0 +1,3 @@
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    git \
+    && apt-get clean && rm -rf /var/lib/apt/lists/* 
