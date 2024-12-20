20db9fa5bab7eb5e791dd18de737093ecf274f25
Austin Gregg-Smith <blooop@gmail.com>
2024-12-20 12:47:51 +0000

    add docstring

--- deps_rocker/uv.py
+++ deps_rocker/uv.py
@@ -3,6 +3,8 @@ from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 
 
 class UV(SimpleRockerExtension):
+    """Add the uv package manager to your docker image"""
+
     name = "uv"
 
     def required(self, cliargs):
