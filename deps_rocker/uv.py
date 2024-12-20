d334d5f80753d375662f54eb25c594b517610c3f
Austin Gregg-Smith <blooop@gmail.com>
2024-12-20 12:39:15 +0000

    add todo for uv cache

--- deps_rocker/uv.py
+++ deps_rocker/uv.py
@@ -10,9 +10,10 @@ class UV(SimpleRockerExtension):
 
     def invoke_after(self, cliargs):
         return set(["curl"])
-
-    def get_docker_args(self, cliargs):
-        # abs = Path("$HOME/.cache/uv").absolute().as_posix()
-        abs = "$HOME/.cache/uv"
-        return f"-v {abs}:{abs}"
+    
+    #TODO enable use of cache on host machine
+    # def get_docker_args(self, cliargs):
+    #     # abs = Path("$HOME/.cache/uv").absolute().as_posix()
+    #     abs = "$HOME/.cache/uv"
+    #     return f"-v {abs}:{abs}"
 
