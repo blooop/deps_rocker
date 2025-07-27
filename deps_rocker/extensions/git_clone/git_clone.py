2efcbd29369a10313e997f25c6760dded6661c1d
Austin Gregg-Smith <blooop@gmail.com>
2025-07-27 12:51:17 +0000

    wip on deps

--- deps_rocker/extensions/git_clone/git_clone.py
+++ deps_rocker/extensions/git_clone/git_clone.py
@@ -5,3 +5,11 @@ class GitClone(SimpleRockerExtension):
     """Adds support for git cloning"""
 
     name = "git_clone"
+
+    # deps = ["git"]
+
+    # def invoke_after(self, cliargs):
+    #     return set(["git"])
+
+    # def required(self, cliargs):
+    #     return set(["git"])
