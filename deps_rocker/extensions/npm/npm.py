5d14cf3b4c56cb1eb0c6843b10e836739615ca79
Austin Gregg-Smith <blooop@gmail.com>
2025-10-05 14:25:11 +0100

    update npm extension to use builder pattern

--- deps_rocker/extensions/npm/npm.py
+++ deps_rocker/extensions/npm/npm.py
@@ -1,8 +1,24 @@
 from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 
 
+
+
+
 class Npm(SimpleRockerExtension):
     """Install npm using nvm (Node Version Manager)"""
 
     name = "npm"
     depends_on_extension = ("curl",)
+
+    empy_args = {
+        "node_version": "24.9.0",
+        "npm_version": "11.6.1",
+        "builder_output_dir": "/opt/deps_rocker/npm",
+        "builder_stage": "npm_builder",
+    }
+
+    empy_builder_args = {
+        "node_version": "24.9.0",
+        "builder_output_dir": "/opt/deps_rocker/npm",
+        "builder_stage": "npm_builder",
+    }
