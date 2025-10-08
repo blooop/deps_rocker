6d16e574337edb868684ad812da5637ca4f85bcd
Austin Gregg-Smith <blooop@gmail.com>
2025-10-08 21:08:32 +0100

    Update deps_rocker/extensions/neovim/neovim.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- deps_rocker/extensions/neovim/neovim.py
+++ deps_rocker/extensions/neovim/neovim.py
@@ -13,5 +13,4 @@ class NeoVim(SimpleRockerExtension):
         home = str(Path.home())
         nvim_config = f"{home}/.config/nvim"
         vim_config = f"{home}/.vim"
-        args = f" -v {nvim_config}:/root/.config/nvim -v {vim_config}:/root/.vim "
-        return args
+        return f" -v {nvim_config}:/root/.config/nvim -v {vim_config}:/root/.vim "
