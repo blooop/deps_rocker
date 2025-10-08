41669803841fe5b704ef0375e985b54cc4d0072d
Austin Gregg-Smith <blooop@gmail.com>
2025-10-08 18:17:58 +0100

    update neovim config

--- deps_rocker/extensions/neovim/neovim.py
+++ deps_rocker/extensions/neovim/neovim.py
@@ -2,7 +2,16 @@ from deps_rocker.simple_rocker_extension import SimpleRockerExtension
 
 
 class NeoVim(SimpleRockerExtension):
-    """Add neovim to your docker image"""
+    """Install the latest stable Neovim and mount host configs (~/.config/nvim, ~/.vim)"""
 
     name = "neovim"
-    apt_packages = ["neovim"]
+    depends_on_extension = ("curl",)
+
+
+    def get_docker_args(self, cliargs):
+        from pathlib import Path
+        home = str(Path.home())
+        nvim_config = f"{home}/.config/nvim"
+        vim_config = f"{home}/.vim"
+        args = f" -v {nvim_config}:/root/.config/nvim -v {vim_config}:/root/.vim "
+        return args
