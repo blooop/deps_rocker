1e5dff0ec378bcb14582baab1c007189a5496363
Austin Gregg-Smith <blooop@gmail.com>
2025-11-01 14:12:47 +0000

    update pixi.lock

--- test/test_package/setup.py
+++ test/test_package/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='test_package',
-    version='0.0.1',
-    packages=['test_package'],
+    name="test_package",
+    version="0.0.1",
+    packages=["test_package"],
     install_requires=[],
 )
